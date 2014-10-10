b0ab73147a14cb3ed50fef2fe3a8073e29c9f57e
deependerrathore <deepak.republic@gmail.com>
2014-10-10 20:03:55 +0530

    While Loop

---
+++ TutorialsPointPrograms/src/While.java
@@ -0,0 +1,12 @@
+//Key point of the While loop is that the loop might not ever run
+public class While {
+	
+	public static void main(String[] args) {
+		int x = 10;
+		while(x < 20){
+			System.out.println("Value of x : " + x );
+			x++;
+		}
+	}
+
+}

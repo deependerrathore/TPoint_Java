4e890b4b23b318632223738c86131cd22e1aa882
deependerrathore <deepak.republic@gmail.com>
2014-10-10 16:51:39 +0530

    How to access instance variable and method of a class

--- TutorialsPointPrograms/src/CreatingAnObject.java
+++ TutorialsPointPrograms/src/CreatingAnObject.java
@@ -1,11 +1,33 @@
 
 public class CreatingAnObject {
+	
+	int myAge;
+	
 	public CreatingAnObject(String name){
 		//This constructor has one parameter name
-		System.out.println("The name is : "+name );
+		System.out.println("Passed name is : "+name );
 	}
 	
+
+	public void setAge(int age){
+		myAge = age;
+	}
+	
+	public int getAge() {
+		System.out.println("My age is : " + myAge);
+		return myAge;
+	}
 	public static void main(String[] args) {
+		
+		//Object Creation
 		CreatingAnObject object = new CreatingAnObject("Deepak");
+		
+		//Call to set myAge from setAge() method
+		object.setAge(23);
+		
+		//Call another method to get Age detail
+		object.getAge();
+		
+		System.out.println("Variable Value : " + object.myAge);
 	}
 }

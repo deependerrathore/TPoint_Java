ad27608f8c330ef96e3fa5134f953effe32b29fb
deependerrathore <deepak.republic@gmail.com>
2014-10-10 16:15:08 +0530

    Comments in java

---
+++ TutorialsPointPrograms/src/CommentInJava.java
@@ -0,0 +1,11 @@
+public class CommentInJava {
+	/*
+	 * This is the multiline comment
+	 * Here we can put many lines continous.
+	 */
+	//This is a single line commit
+	/*this is also a single line comment*/
+	public static void main(String[] args) {
+		System.out.println();
+	}
+}

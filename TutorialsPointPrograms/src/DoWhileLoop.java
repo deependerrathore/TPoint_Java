f591b3763f189289d8aeba2449013387e6f039f4
deependerrathore <deepak.republic@gmail.com>
2014-10-10 20:05:33 +0530

    Do....while loop

---
+++ TutorialsPointPrograms/src/DoWhileLoop.java
@@ -0,0 +1,12 @@
+//Do...while loop guaranteed to execute at leart on time.
+public class DoWhileLoop {	
+	
+	public static void main(String[] args) {
+		int x = 10;
+		do{
+			System.out.println(x);
+			x++;
+		}while(x<20);
+	}
+
+}

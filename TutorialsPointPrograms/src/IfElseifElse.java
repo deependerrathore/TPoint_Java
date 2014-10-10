15320655825b60e5e101b53bc4649b14d3cfbd8a
deependerrathore <deepak.republic@gmail.com>
2014-10-10 22:32:59 +0530

    Decision Making : IF...ELSE IF ....ELSE

---
+++ TutorialsPointPrograms/src/IfElseifElse.java
@@ -0,0 +1,13 @@
+
+public class IfElseifElse {
+	public static void main(String[] args) {
+		int x = 17;
+		if(x <15){
+			System.out.println("if");
+		}else if(x < 20	){
+			System.out.println("Else if");
+		}else{
+			System.out.println("ELSE");
+		}
+	}
+}

d70a3827bb6bf2394a0aad00325d2e4c6f1f9ba6
deependerrathore <deepak.republic@gmail.com>
2014-10-10 22:29:14 +0530

    Decision Making : IF ELSE Statement

---
+++ TutorialsPointPrograms/src/IfElseDecison.java
@@ -0,0 +1,11 @@
+
+public class IfElseDecison {
+	public static void main(String[] args) {
+		int x = 10;
+		if(x <20){
+			System.out.println("This is IF statement");
+		}else{
+			System.out.println("This is ELSE staement");
+		}
+	}
+}

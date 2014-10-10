0a6da068e99e1c96d57fbec87d585768f25b6e12
deependerrathore <deepak.republic@gmail.com>
2014-10-10 16:43:13 +0530

    Creating an Object

---
+++ TutorialsPointPrograms/src/CreatingAnObject.java
@@ -0,0 +1,11 @@
+
+public class CreatingAnObject {
+	public CreatingAnObject(String name){
+		//This constructor has one parameter name
+		System.out.println("The name is : "+name );
+	}
+	
+	public static void main(String[] args) {
+		CreatingAnObject object = new CreatingAnObject("Deepak");
+	}
+}

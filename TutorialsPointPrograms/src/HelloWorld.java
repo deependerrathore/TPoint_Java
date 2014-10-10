5e2c5ce376f182dbf1a48ccb62c64633687ba788
deependerrathore <deepak.republic@gmail.com>
2014-10-10 16:11:09 +0530

    This is the first Program print Hello world

--- TutorialsPointPrograms/src/HelloWorld.java
+++ TutorialsPointPrograms/src/HelloWorld.java
@@ -1,6 +1,8 @@
-
 public class HelloWorld {
+	/*tHIS IS MY FIRST JAVA PROGRAM .
+	 * This will print "Hello World " as the output.
+	 */
 	public static void main(String[] args) {
-		System.out.println("Deepak");
+		System.out.println("Hello World");//print hello world
 	}
 }

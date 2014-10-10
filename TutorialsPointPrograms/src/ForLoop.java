10df57728ac9f8cd87baf1a2d50af37a5238729d
deependerrathore <deepak.republic@gmail.com>
2014-10-10 20:07:21 +0530

    For loop

---
+++ TutorialsPointPrograms/src/ForLoop.java
@@ -0,0 +1,12 @@
+/*
+ * for(initialization ; condition ; increment/decrement){
+ * }
+ */
+public class ForLoop {
+
+	public static void main(String[] args) {
+		for(int i = 10 ; i<20; i++){
+			System.out.println(i);
+		}
+	}
+}

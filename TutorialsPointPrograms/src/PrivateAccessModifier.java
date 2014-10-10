491864c371179db3c260f1454ee5d13215f08e78
deependerrathore <deepak.republic@gmail.com>
2014-10-10 18:43:07 +0530

    Private variable

---
+++ TutorialsPointPrograms/src/PrivateAccessModifier.java
@@ -0,0 +1,11 @@
+public class PrivateAccessModifier {
+	
+	private String format;	// Private Instance variable
+	public String getFormat(){
+		return format;
+	}
+	
+	public void setFormat(String format){
+		this.format = format;
+	}
+}

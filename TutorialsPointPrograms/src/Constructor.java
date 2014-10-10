cb12e6fe3a092c8ba5a16e1eecbc9c87417a9c1f
deependerrathore <deepak.republic@gmail.com>
2014-10-10 16:20:56 +0530

    Constructor in Java

---
+++ TutorialsPointPrograms/src/Constructor.java
@@ -0,0 +1,12 @@
+public class Constructor {
+	/*constructor have same name as class.Every class has a constructor.
+	 * Each time a new object is created , at least one constructor will be invoked.
+	 * A class can have more than on constructor.
+	 */
+	public Constructor(){
+		
+	}
+	public Constructor(String name){
+		//This contructor take one parameter, name
+	}
+}
